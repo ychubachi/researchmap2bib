cffae32fa1f0f5e446b993d0c8a23dc823c2456c
Yoshihide Chubachi <yoshi@chubachi.net>
2016-08-23 23:53:30 +0900

    Almost done (not checked by LaTeX)

--- test/researchmap2bib_test.rb
+++ test/researchmap2bib_test.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require 'test_helper'
+require 'researchmap2bib'
 
 class Researchmap2bibTest < Minitest::Test
   def setup
@@ -17,9 +18,14 @@ class Researchmap2bibTest < Minitest::Test
     }
   end
 
-  # def test_read_paper
-  #   assert_nil @s.read_paper
-  # end
+  def test_read_paper
+    r = File.open('test/ychubachi/paper.xml') do |file|
+      xml = file.read
+      @s.read_paper(xml)
+    end
+    assert_equal 72, r.count
+    assert_instance_of Researchmap2bib::Entry, r[0]
+  end
 
   def test_read_entry
     doc = REXML::Document.new(<<XML)
@@ -41,6 +47,49 @@ class Researchmap2bibTest < Minitest::Test
 XML
     entry = doc.elements["entry"]
     r = @s.read_entry(entry)
-    assert_equal "#<struct Researchmap2bib::Entry title=\"Title\", author=\"Name\", summary=\"Summary\", journal=\"Journal\", publisher=\"Publisher\", publicationName=\"PublicationName\", volume=\"31\", number=nil, startingPage=\"121\", endingPage=\"125\", publicationDate=\"20140900\", referee=false, language=\"ja\", paperType=nil>", r.to_s
+    assert_equal "#<struct Researchmap2bib::Entry title=\"Title\", author=\"Name\", summary=\"Summary\", journal=\"Journal\", publisher=\"Publisher\", publicationName=\"PublicationName\", volume=\"31\", number=nil, startingPage=\"121\", endingPage=\"125\", publicationDate=\"20140900\", referee=false, language=\"ja\", paperType=\"0\">", r.to_s
+  end
+
+  def test_write_bibliography_entry
+    e = Researchmap2bib::Entry.new('Title', 'Author Name1, Author Name2')
+    e.publicationDate="20140900"
+    @s.write_bibliography_entry(e)
+  end
+
+  def test_to_hankaku
+    assert_equal '1234', @s.to_hankaku('１２３４')
+    assert_equal 'abc', @s.to_hankaku('ａｂｃ')
+    assert_equal ',', @s.to_hankaku('，')
+    assert_equal ',', @s.to_hankaku('、')
+    assert_equal ' ', @s.to_hankaku('　')
+  end
+
+  def test_concatenate_authors
+    assert_equal 'First Last', @s.concatenate_authors('First Last')
+    assert_equal 'First1 Last1 and First2 Last2',
+                 @s.concatenate_authors('First1 Last1, First2 Last2')
+    assert_equal 'First1 Last1 and First2 Last2 and First3 Last3',
+                 @s.concatenate_authors('First1 Last1, First2 Last2 , First3 Last3')
+    assert_equal '山田 太郎 and 田中 花子',
+                 @s.concatenate_authors('山田 太郎 ,田中 花子')
+  end
+
+  def test_first_author
+    assert_equal 'John Smith', @s.first_author('John Smith')
+    assert_equal 'John Smith', @s.first_author('John Smith , Kathy River')
+    assert_equal '山田 太郎', @s.first_author('山田 太郎, 田中 花子')
+    assert_equal '山田 太郎', @s.first_author('山田 太郎, 田中 花子, 斎藤 次郎')
+  end
+
+  def test_family_name
+    assert_equal 'Smith', @s.family_name('John Smith')
+    assert_equal '山田', @s.family_name('山田 太郎')
+    assert_equal '山田太郎', @s.family_name('山田太郎')
+  end
+
+  def test_generate_key
+    assert_equal 'Smith1608', @s.generate_key('John Smith, Kathy River', '20160800')
+    assert_equal 'Smith16', @s.generate_key('John Smith, Kathy River', '20160000')
+    assert_equal '山田1608', @s.generate_key('山田 太郎, 田中 花子', '20160800')
   end
 end
