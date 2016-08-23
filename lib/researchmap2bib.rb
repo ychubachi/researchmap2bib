756d54d027b5bfb7a38d9fb71cb30e02f3e92491
Yoshihide Chubachi <yoshi@chubachi.net>
2016-08-23 20:03:17 +0900

    Read one entry

--- lib/researchmap2bib.rb
+++ lib/researchmap2bib.rb
@@ -1,5 +1,67 @@
+# coding: utf-8
 require "researchmap2bib/version"
 
+require 'zip'
+require 'rexml/document'
+require 'rexml/parsers/ultralightparser'
+require 'pp'
+
 module Researchmap2bib
-  # Your code goes here...
+  Entry = Struct.new(
+    :title, :author, :summary, :journal, :publisher, :publicationName,
+    :volume, :number, :startingPage, :endingPage, :publicationDate,
+    :referee, :language, :paperType,
+  )
+  
+  Article = Struct.new(
+    :author, :title, :journal, :year,
+    :pages, :month, :note, :key,
+    :volume, :number,
+  )
+  
+  InProceedings = Struct.new(
+    :author, :title, :booktitle, :year,
+    :pages, :month, :note, :key,
+    :editor, :organization,
+    :publisher, :address,
+  )
+
+  class ResearchmapReader
+    def read_researchmap(file_name)
+      Zip::File.open(file_name) do |zip_file|
+        entry = zip_file.glob('*\/paper.xml').first
+        read_paper(entry.get_input_stream.read)
+      end
+    end
+
+    def read_paper(xml)
+      doc = REXML::Document.new(xml)
+      entries = REXML::XPath.each(doc, '/feed/entry') do |entry|
+        read_entry(entry)
+      end
+    end
+
+    def read_entry(entry)
+      title           = entry.elements["title"].text
+      author          = entry.elements["author"].elements["name"].text
+      summary         = entry.elements["summary"].text
+      journal         = entry.elements["rm:journal"].text
+      publisher       = entry.elements["rm:publisher"].text
+      publicationName = entry.elements["rm:publicationName"].text
+      volume          = entry.elements["rm:volume"].text
+      number          = entry.elements["rm:number"].text
+      startingPage    = entry.elements["rm:startingPage"].text
+      endingPage      = entry.elements["rm:endingPage"].text
+      publicationDate = entry.elements["rm:publicationDate"].text
+      referee         = entry.elements["rm:referee"] ? true : false
+      language        = entry.elements["rm:language"].text
+      paperType       = entry.elements["rm:paperType"].text
+        
+      Entry.new(
+        title, author, summary, journal, publisher, publicationName,
+        volume, number, startingPage, endingPage, publicationDate,
+        referee, language, paperType,
+      )
+    end
+  end
 end
--- test/researchmap2bib_test.rb
+++ test/researchmap2bib_test.rb
@@ -1,7 +1,46 @@
+# coding: utf-8
 require 'test_helper'
 
 class Researchmap2bibTest < Minitest::Test
+  def setup
+    @s = Researchmap2bib::ResearchmapReader.new
+  end
+  
   def test_that_it_has_a_version_number
     refute_nil ::Researchmap2bib::VERSION
+  end
+
+  def test_read_researchmap
+    assert_nil = @s.read_researchmap('test/ychubachi.zip')
+    assert_raises(Zip::Error) {
+      @s.read_researchmap('test/nosuchfile.zip')
+    }
+  end
+
+  # def test_read_paper
+  #   assert_nil @s.read_paper
+  # end
+
+  def test_read_entry
+    doc = REXML::Document.new(<<XML)
+<entry xmlns:rm="http://api.researchmap.jp/ns/" rm:type="paper" rm:disclose="public">
+  <title>Title</title>
+  <author><name>Name</name></author>
+  <summary>Summary</summary>
+  <rm:journal>Journal</rm:journal>
+  <rm:publisher>Publisher</rm:publisher>
+  <rm:publicationName>PublicationName</rm:publicationName>
+  <rm:volume>31</rm:volume>
+  <rm:number/>
+  <rm:startingPage>121</rm:startingPage>
+  <rm:endingPage>125</rm:endingPage>
+  <rm:publicationDate>20140900</rm:publicationDate>
+  <rm:language>ja</rm:language>
+  <rm:paperType><id>0</id><name/></rm:paperType>
+</entry>
+XML
+    entry = doc.elements["entry"]
+    r = @s.read_entry(entry)
+    assert_equal "#<struct Researchmap2bib::Entry title=\"Title\", author=\"Name\", summary=\"Summary\", journal=\"Journal\", publisher=\"Publisher\", publicationName=\"PublicationName\", volume=\"31\", number=nil, startingPage=\"121\", endingPage=\"125\", publicationDate=\"20140900\", referee=false, language=\"ja\", paperType=nil>", r.to_s
   end
 end
